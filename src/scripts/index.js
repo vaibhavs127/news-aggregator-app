7a1adb1f01d9729f03ff04e6af8a7586d256cfca
Vaibhav <59925145+vaibhavs127@users.noreply.github.com>
2020-05-03 16:05:10 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -63,7 +63,7 @@ $(document).ready(function(){
                 $("#news-articles").html("<h1 class="not-found">No article was found based on the search.</h1>");
             } 
           }   
-        }); 
+        } 
       }
     });
 });
